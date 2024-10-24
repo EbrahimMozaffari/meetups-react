97376042ee567b98b34c7984e268b60d30fe2185
Ebrahim Mozaffari <ebrahim.mozafary@gmail.com>
2024-10-24 15:34:58 +0100

    show some dummy data item add layout and use wrapper children

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritePage from "./pages/Favorite";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <>
-      <Switch>
-        <Route path="/" exact>
-          <AllMeetupsPage />
-        </Route>
-        <Route path="/new-meetup">
-          <NewMeetupPage />
-        </Route>
-        <Route path="/favorite">
-          <FavoritePage />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <AllMeetupsPage />
+          </Route>
+          <Route path="/new-meetup">
+            <NewMeetupPage />
+          </Route>
+          <Route path="/favorite">
+            <FavoritePage />
+          </Route>
+        </Switch>
+      </Layout>
     </>
   );
 }
---
+++ src/components/ui/Card.js
@@ -0,0 +1,14 @@
+import classes from './Card.module.css'
+
+
+function Card({children}) {
+    return(
+        <>
+            <div className={classes.card}>
+                {children}
+            </div>
+        </>
+    )
+}
+
+export default Card;
